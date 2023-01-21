2a724dfd361b7fb0a78555e56989e9ce1d964485
whatever <you@example.com>
2023-01-21 08:49:11 +0100

    empty commit

---
+++ app2.js
@@ -0,0 +1,4 @@
+function show2 () {
+    document.querySelector('.hamburger2').classList.toggle('open')
+    document.querySelector('.navigation2').classList.toggle('active')
+}
---
+++ app3.js
@@ -0,0 +1,4 @@
+function show3 () {
+    document.querySelector('.hamburger3').classList.toggle('open')
+    document.querySelector('.navigation3').classList.toggle('active')
+}
